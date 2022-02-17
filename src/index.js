8fd440e9f6891172b2ee033beb14b8ef6e4b82da
sureyes90 <susanreyes25@gmail.com>
2022-02-17 11:36:09 -0500

    primer comentario

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
-import validator from './validator.js';
 
-console.log(validator);
+import validator from './validator.js';
+function comprobar (){
+document.getElementById ("principal").style.display = "none";
+document.getElementById ("welcome").style.display = "block";
+}
+let btn = document.getElementById ("bienvenidos");
+btn.addEventListener("click",comprobar);
+
+function confirmar (){
+document.getElementById ("welcome").style.display = "none";
+document.getElementById ("date").style.display = "block";
+}
+let  btn1 = document.getElementById("ingresar");
+btn1.addEventListener("click",confirmar);
+
+function validar (){
+    document.getElementById ("date").style.display = "none";
+
+let  cardNumber= document.getElementById ("ingreso").value;
+validator.isValid(cardNumber);
+let numberValidator =  validator.isValid(cardNumber);
+alert(numberValidator);
+}
+let  btn2 = document.getElementById("buttonbuy");
+btn2.addEventListener("click",validar);
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,41 @@
-const validator = {
-  // ...
-};
 
+const validator = {
+  isValid: function (cardNumber){
+// pasar los numeros a un array
+
+let newArray = Array.from(cardNumber);
+
+// los string del array a  numeros
+let secondArray =[];
+for(let i=0; i<newArray.length; i++){
+  secondArray.push(parseInt(newArray[i]));
+}
+//invertir los numeros del array
+let reverseArray=secondArray.reverse();
+let sumaPares = 0;
+let sumaImpares=0;
+//ubicar los numeros de indice par
+for(let i=1 ; i<reverseArray.length; i+=2){
+//multiplicar por 2 los numeros encontrados
+  let positionPar=reverseArray[i]*2;
+  if (positionPar>=10){
+  sumaPares= sumaPares+ 1+ positionPar%10;
+  }
+  else sumaPares= sumaPares + positionPar;
+}
+for (let i=0;i<reverseArray.length;i+=2){
+sumaImpares=sumaImpares + reverseArray[i];
+}
+let sumaTotal=sumaPares+sumaImpares;
+let respuesta;
+if(sumaTotal%10==0){
+respuesta=true;
+} else respuesta=false;
+return respuesta;
+  },
+  maskify :function (cardNumber){
+  return cardNumber;
+  },
+};
 export default validator;
+
